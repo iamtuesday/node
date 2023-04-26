12f45e75f43b51ee438ef1c4195a2eeff08b06cf
iamtuesday <inf.roycd@gmail.com>
2023-04-25 21:37:15 -0500

    feat: add connection database

--- basic-ts/README.md
+++ basic-ts/README.md
@@ -14,3 +14,7 @@ pnpm i @types/node @types/express -D
 pnpm i supertest jest ts-jest @types/jest @types/supertest -d
 
 pnpm ts-jest config:init
+
+pnpm i express zod config cors express mongoose pino pino-pretty dayjs bcrypt jsonwebtoken lodash nanoid
+
+pnpm i @types/body-parser @types/config @types/cors @types/express @types/node @types/pino @types/bcrypt @types/jsonwebtoken @types/lodash @types/nanoid ts-node-dev typescript -D
--- basic-ts/src/index.ts
+++ basic-ts/src/index.ts
@@ -1,4 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+import logger from "./utils/logger";
+
+
+import config from "config";
+import responseTime from "response-time";
+import connect from "./utils/connect";
 
 const app = express();
 const port = 3000;
@@ -11,6 +20,8 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
   res.json({ message: "Hello World!" });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, async () => {
+  logger.info(`App is running at http://localhost:${port}`);
+
+  await connect();
 });
